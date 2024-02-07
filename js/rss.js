ffeea62899574d78590d6b0664d28be8168a28b9
Bernat <bernat13@gmail.com>
2024-02-07 08:40:25 +0100

    ejemplo h2 titulo

--- js/rss.js
+++ js/rss.js
@@ -2,12 +2,25 @@ fetch('rss.xml')
     .then(response => response.text())
     .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
     .then(data =>  {
+       // console.log("data:",data)
         var rss = data.querySelector('rss');
-
             // en data tengo el contenido del xml SIEMPRE que el RSS esté en mi mismo servidor.
             // Para eso, usaremos el live server de Visual Studio Code.
-            console.log(rss);
-    }
+         //   console.log("rss:",rss);
 
 
-    );
+            var items= rss.querySelectorAll("item");
+           // console.log("items:" + items[0].innerHTML)
+            var html = ""
+            for (var i = 0 ; i< items.length;i++)
+            {
+                var titulo = items[i].querySelector("title")
+                console.log(titulo.innerHTML);
+                html =html+ "<h2>" + titulo.innerHTML + "</h2>";
+
+            }
+
+
+            var contenido = document.querySelector("#contenido")
+            contenido.innerHTML=html;
+    });
